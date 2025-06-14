2bd109eea9a3f6145d3b758469712e5fbb31e171
AidanD1 <aidandunn2021@gmail.com>
2025-06-13 21:29:19 -0500

    Added goals

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
-const learning = {};
-learning.topic = "JS";
-learning.learningGoals = [
+const body = document.querySelector("body");
+const success = document.querySelector(".success");
+const dance = document.querySelector(".dance");
+
+const learning = {
+topic: "JS",
+learningGoals: [
   "Build JS Programs",
   "Learn Python",
   "Understand AI",
   "Have fun programming",
-];
+],
 
-learning.category = "Front End Development";
-
-learning.topicImportance = "High";
+category: "Front End Development",
+topicImportance: "high",
+hoursThisWeek: 2,
+  weeklyHourGoal: 7,
+  achievedStudyGoal: false,
+  addStudyTime: function (hours) {
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    }
+  },
+celebrate: function() {
+  body.classList.add("celebrate");
+  success.classList.remove("hide");
+  dance.classList.remove("hide");
+}
+};
 
 console.log(learning);
 
-learning.topic = "JavaScript";
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+countElement.innerText = `✔️ I have learning ${learning.learningGoals.length} learning goals.`;
 
-learning.learningGoals.splice(1, 1);
-
-console.log(learning);
-
-topicElement.innerText = `I'm learning ${learning.topic}.`;
 topicElement.classList.remove("hide");
+countElement.classList.remove("hide");
 
-courtElement.innerText = `I have learning ${learning.learnGoals.length} learning goals.`;
-courtElement.classList.remove("hide");
+
+
+learning.addStudyTime(5);
